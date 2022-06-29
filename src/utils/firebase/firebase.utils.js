29c3d733511d1cf905ec6eb6d78824d2fccb8dce
Adit <adityanuar@gmail.com>
2022-06-29 11:05:28 +0700

    firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCmO3INF2GKDMRcaEYWnEYMA5aHdwMNiJU",
+  authDomain: "clothing-adityanuar.firebaseapp.com",
+  projectId: "clothing-adityanuar",
+  storageBucket: "clothing-adityanuar.appspot.com",
+  messagingSenderId: "2974366974",
+  appId: "1:2974366974:web:e2f1d8db11cc25a86e79af",
+  measurementId: "G-B6ZGHX6KHZ",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot.exists());
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+  return userDocRef;
+};
